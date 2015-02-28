3122108c64a2147c9c33d554153ec4e9821ce6e8
changhai <changhai@baidu.com>
2015-02-28 15:49:50 +0800

    init

--- src/main/g8/project/build.scala
+++ src/main/g8/project/build.scala
@@ -1,52 +1,25 @@
 import sbt._
 import Keys._
-import org.scalatra.sbt._
-import org.scalatra.sbt.PluginKeys._
-import sbtassembly.AssemblyKeys._
-import sbtassembly.AssemblyPlugin._
 
 object $name;format="Camel"$Build extends Build {
   val Organization = "$organization$"
   val Name = "$name$"
   val Version = "$version$"
   val ScalaVersion = "$scala_version$"
-  val ScalatraVersion = "$scalatra_version$"
-  val SlickVersion = "$slick_version$"
 
   lazy val project = Project (
     "$name;format="norm"$",
     file("."),
-    settings = Defaults.defaultSettings ++ ScalatraPlugin.scalatraWithJRebel ++ assemblySettings ++ Seq(
+    settings = Seq(
       organization := Organization,
       name := Name,
       version := Version,
-      scalaVersion := ScalaVersion,
       scalacOptions ++= Seq("-deprecation", "-feature"),
-      resolvers += Classpaths.typesafeReleases,
-      assemblyMergeStrategy in assembly := {
-        case x => (assemblyMergeStrategy in assembly).value(x)
-      },
-      resourceGenerators in Compile <+= (resourceManaged, baseDirectory) map {
-        (managedBase, base) =>
-          val webappBase = base / "src" / "main" / "webapp"
-          for {
-            (from, to) <- webappBase ** "*" pair rebase(webappBase, managedBase / "main" / "webapp")
-          } yield {
-            Sync.copy(from, to)
-            to
-          }
-      },
+      resolvers ++= Seq(Resolver.url("nexus-ivy", url("http://cq01-rdqa-pool106.cq01.baidu.com:8081/nexus/content/groups/public/"))(Resolver.ivyStylePatterns),
+            "nexus-m2" at "http://cq01-rdqa-pool106.cq01.baidu.com:8081/nexus/content/groups/public/",
+            Classpaths.typesafeReleases),
       libraryDependencies ++= Seq(
-        "org.scalatra" %% "scalatra" % ScalatraVersion,
-        "org.scalatra" %% "scalatra-swagger" % ScalatraVersion,
-        "org.scalatra" %% "scalatra-json" % ScalatraVersion,
-        "org.json4s"   %% "json4s-jackson" % "3.2.10",
-        "com.typesafe.slick" %% "slick" % SlickVersion,
-        "com.typesafe.slick" %% "slick-codegen" % SlickVersion,
-        "org.scalatra" %% "scalatra-specs2" % ScalatraVersion % "test",
-        "ch.qos.logback" % "logback-classic" % "1.1.2" % "runtime",
-        "org.eclipse.jetty" % "jetty-webapp" % "8.1.8.v20121106" % "container;compile;test",
-        "org.eclipse.jetty.orbit" % "javax.servlet" % "3.0.0.v201112011016" % "container;provided;test" artifacts (Artifact("javax.servlet", "jar", "jar"))
+        "ch.qos.logback" % "logback-classic" % "1.1.2"
       )
     )
   )
